b5b5fbb35568b1e24cf58b6a739841a1693518f0
Riddhi Lamba <ridlam24@bergen.org>
2023-11-17 15:16:14 -0500

    nov 17

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             isOnGround = false;
-            playerAnim.SetTrigger("Jump--Jump");
+            playerAnim.SetTrigger("Jump");
         }
 
     }
